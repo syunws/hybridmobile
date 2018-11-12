8a11a298288cbf979ec8296fc2fdb8b1d8d57da9
sooylee <syunws@gmail.com>
2018-11-12 09:33:34 +0900

    Redux Example

---
+++ redux/redux-exam/src/component/Counter.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const Counter = (value) => {
+    return (
+        <div>
+            <h1>{value}</h1>
+            <button>+</button>
+            <button>-</button>
+        </div>
+    );
+}
+
